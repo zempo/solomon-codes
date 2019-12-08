ed23e901c214e478c5d7be7c6efb680b17410908
zempo <zelenkosolomon@gmail.com>
2019-12-07 23:11:56 -0800

    update layout

--- dist/templates/nav.js
+++ dist/templates/nav.js
@@ -1,17 +1,11 @@
 window.onload = () => {
   let nav = `<nav>
-    <div class="logo-link">
-      <a href="index.html">Logo</a>
-    </div>
     <div class="menu">
-      <div class="menu-icon">
-        &#9776;
-      </div>
       <div class="menu-items">
         <ul>
-          <li><a href="#about">Blog</a></li>
-          <li><a href="#snippets_lab">Generators</a></li>
-          <li><a href="#projects">Projects</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#snippets">Snippets</a></li>
+          <li><a href="#project">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </div>
