027c8dc754264d0a144114ec4edf3ebdb2e4dafc
zempo <zelenkosolomon@gmail.com>
2020-02-12 22:06:31 -0800

    update layout

--- dist/js/layout.js
+++ dist/js/layout.js
@@ -65,7 +65,7 @@ const currentSnippets = [
   },
   {
     type: "animation",
-    title: "Lively Greeting Cards",
+    title: "JTO Cards",
     link: "https://codepen.io/zemposPen/pen/PowLbBd"
   },
   {
